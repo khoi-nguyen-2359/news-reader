826ede9316d1b7cb9d2a102c1f8e8a55d87821d4
Khoi Nguyen <akhoi90@gmail.com>
2020-09-01 16:31:56 +0700

    Dont use static map when setting dark mode

--- app/src/main/java/akio/apps/newsreader/util/DarkModeHelper.java
+++ app/src/main/java/akio/apps/newsreader/util/DarkModeHelper.java
@@ -2,43 +2,22 @@ package akio.apps.newsreader.util;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import akio.apps.newsreader.R;
 
 public class DarkModeHelper {
-    @Nullable
-    private static Map<String, Integer> mapDarkModeValues;
-
     public static void applyDarkModePreference(Context context, SharedPreferences sharedPreferences, String darkModePrefKey) {
-        initDarkModeValueMap(context);
-
         String darkModePrefValue = sharedPreferences.getString(darkModePrefKey, null);
-        if (darkModePrefValue == null) {
-            return;
+        Resources resources = context.getResources();
+        if (resources.getString(R.string.pref_value_dark_mode_night_system).equals(darkModePrefValue)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        } else if (resources.getString(R.string.pref_value_dark_mode_night_no).equals(darkModePrefValue)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (resources.getString(R.string.pref_value_dark_mode_night_yes).equals(darkModePrefValue)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }
-
-        Integer darkModeSettingValue = mapDarkModeValues.get(darkModePrefValue);
-        if (darkModeSettingValue == null) {
-            return;
-        }
-
-        AppCompatDelegate.setDefaultNightMode(darkModeSettingValue);
-    }
-
-    private static void initDarkModeValueMap(Context context) {
-        if (mapDarkModeValues != null) {
-            return;
-        }
-
-        mapDarkModeValues = new HashMap<>();
-        mapDarkModeValues.put(context.getResources().getString(R.string.pref_value_dark_mode_night_system), AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-        mapDarkModeValues.put(context.getResources().getString(R.string.pref_value_dark_mode_night_no), AppCompatDelegate.MODE_NIGHT_NO);
-        mapDarkModeValues.put(context.getResources().getString(R.string.pref_value_dark_mode_night_yes), AppCompatDelegate.MODE_NIGHT_YES);
     }
 }
